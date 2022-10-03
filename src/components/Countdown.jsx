1927d0bbd3a04277feb822c2c3d23adc854ba49d
Michel Pomerantzeff <micheldauniao@hotmail.com>
2022-10-03 22:27:26 +0100

    finished countdown page

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -13,28 +13,35 @@ function Countdown(props) {
     const [min, setMin] = useState(0)
     const [hour, setHour] = useState(0)
 
-    // const [time, setTime] = useState({
-    //     sec: 0,
-    //     min: 0,
-    //     hour: 0
-    // })
+    const [isVisible, setIsVisible] = useState(true)
 
     const [timerOn, setTimerOn] = useState(false)
 
+    useEffect(() => {
+        sec + min + hour === 0 && setTimerOn(prev => false)
+    }, [sec])
 
+
+    useEffect(() => {
+        if (min === 59 && timerOn) {
+            setHour(prevHour => prevHour < 1 ? 23 : prevHour - 1)
+        }
+    }, [min])
+
+    useEffect(() => {
+        if (sec === 59 && timerOn) {
+            setMin(prevMin => prevMin < 1 ? 59 : prevMin - 1)
+        }
+    }, [sec])
 
     useEffect(() => {
         let interval = null
 
         if (timerOn) {
             interval = setInterval(() => {
+
                 setSec(prevSec => prevSec < 1 ? 59 : prevSec - 1)
 
-                setMin(prevMin => prevMin < 1 ? 59 : ((prevMin * 60) - 1) / 60)
-
-                setHour(prevHour => prevHour > 0 && ((prevHour * 60 * 60) - 1) / 60 / 60)
-
-                console.log(sec, min, hour)
             }, 1000)
         } else {
             clearInterval(interval)
@@ -52,35 +59,41 @@ function Countdown(props) {
             </div>
             <div className='countdown'>
                 <div className='hour'>
-                    <button className='arrow-buttons' onClick={() => setHour(prevHour => prevHour < 23 ? prevHour + 1 : 0)}>{up}</button>
+                    { isVisible && !timerOn &&
+                        <button className='arrow-buttons' onClick={() => setHour(prevHour => prevHour < 23 ? prevHour + 1 : 0)}>{up}</button>}
                     <span>{("0" + Math.ceil(hour)).slice(-2)}</span>
-                    <button className='arrow-buttons' onClick={() => setHour(prevHour => prevHour > 0 ? prevHour - 1 : 23)}>{down}</button>
+                    { isVisible && !timerOn &&
+                        <button className='arrow-buttons' onClick={() => setHour(prevHour => prevHour > 0 ? prevHour - 1 : 23)}>{down}</button>}
                 </div>
 
                 <span className='colon'>:</span>
 
                 <div className='min'>
-                    <button className='arrow-buttons' onClick={() => setMin(prevMin => prevMin < 59 ? prevMin + 1 : 0)}>{up}</button>
+                    { isVisible && !timerOn &&
+                        <button className='arrow-buttons' onClick={() => setMin(prevMin => prevMin < 59 ? prevMin + 1 : 0)}>{up}</button>}
                     <span>{("0" + Math.ceil(min)).slice(-2)}</span>
-                    <button className='arrow-buttons' onClick={() => setMin(prevMin => prevMin > 0 ? prevMin - 1 : 59)}>{down}</button>
+                    { isVisible && !timerOn &&
+                        <button className='arrow-buttons' onClick={() => setMin(prevMin => prevMin > 0 ? prevMin - 1 : 59)}>{down}</button>}
                 </div>
 
                 <span className='colon'>:</span>
 
                 <div className='sec'>
-                    <button className='arrow-buttons' onClick={() => setSec(prevSec => prevSec < 59 ? prevSec + 1 : 0)}>{up}</button>
+                    { isVisible && !timerOn &&
+                        <button className='arrow-buttons' onClick={() => setSec(prevSec => prevSec < 59 ? prevSec + 1 : 0)}>{up}</button>}
                     <span>{("0" + (sec)).slice(-2)}</span>
-                    <button className='arrow-buttons' onClick={() => setSec(prevSec => prevSec > 0 ? prevSec - 1 : 59)}>{down}</button>
+                    { isVisible && !timerOn &&
+                        <button className='arrow-buttons' onClick={() => setSec(prevSec => prevSec > 0 ? prevSec - 1 : 59)}>{down}</button>}
                 </div>
 
             </div>
 
             <div className='countdown-buttons'>
 
-                {!timerOn && sec + min + hour === 0 && <button className='start' onClick={() => setTimerOn(true)}>Start</button>}
-                {!timerOn && sec + min + hour === 0 && <button className='set'>Set Time</button>}
+                {!timerOn && <button  className='start' onClick={() => setTimerOn(true)}>Start</button>}
+                {/* {!timerOn && sec + min + hour > 0 && <button onClick={() => setTimerOn(true)} className='resume'>Resume</button>} */}
                 {timerOn && <button className='stop' onClick={() => setTimerOn(false)}>Stop</button>}
-                {!timerOn && sec + min + hour > 0 && <button className='resume'>Resume</button>}
+                {/* {!timerOn && sec + min + hour === 0 && <button onClick={() => setIsVisible(true)} className='set'>Set Time</button>} */}
                 {!timerOn && sec + min + hour > 0 && <button className='reset' onClick={() => {
                     setHour(0)
                     setMin(0)
